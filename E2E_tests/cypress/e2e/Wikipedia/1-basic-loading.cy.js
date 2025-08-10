a3de6db4f891c3ded5cdc5b7f6f4f4ad358c8547
Philipp_Ivanov <128012990+Philaktet@users.noreply.github.com>
2025-08-11 00:46:32 +0300

    добавил ошибку в 1й тест -wkipedia.org

--- E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
+++ E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
@@ -1,7 +1,7 @@
 describe('Wikipedia Smoke Test', () => {
   it('Главная страница открывается', () => {
     // Просто проверяем, что страница загружается
-    cy.visit('https://ru.wikipedia.org', { timeout: 10000 })
+    cy.visit('https://ru.wkipedia.org', { timeout: 10000 })
       .title() // Проверяем title страницы
       .should('include', 'Википедия')
   })
