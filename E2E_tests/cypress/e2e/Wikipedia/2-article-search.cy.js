ebd3e0ff8d4a32146023323290bb026ed6fab75b
Philipp_Ivanov <128012990+Philaktet@users.noreply.github.com>
2025-08-11 01:17:16 +0300

    попытка 4, ошибка 1го теста.

--- E2E_tests/cypress/e2e/Wikipedia/2-article-search.cy.js
+++ E2E_tests/cypress/e2e/Wikipedia/2-article-search.cy.js
@@ -4,6 +4,6 @@ describe('Wikipedia Article Search Test', () => {
     cy.visit('https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0')
     
     // 2. Проверка заголовка
-    cy.get('#firstHeading').should('contain', 'Собакааа')
+    cy.get('#firstHeading').should('contain', 'Собака')
   })
 })
--- E2E_tests/scripts/test-runner.js
+++ E2E_tests/scripts/test-runner.js
@@ -2,16 +2,9 @@ const { exec } = require('child_process');
 const path = require('path');
 
 const cypressBin = path.join(__dirname, '../node_modules/.bin/cypress');
-const command = `"${cypressBin}" run --quiet --parallel --ci-build-id ${
-  process.env.GITHUB_RUN_ID || 'local'
-}`;
+const command = `"${cypressBin}" run --quiet --parallel --ci-build-id ${process.env.GITHUB_RUN_ID || 'local'}`;
 
 exec(command, (error, stdout) => {
-  const errors = stdout.match(/(✖|error:|fail:).*/gi);
-  if (errors?.length) {
-    console.log(`❌ ${errors.length} error(s):\n${errors.join('\n')}`);
-    process.exit(1);
-  } else {
-    console.log('✅ All tests passed');
-  }
+  console.log(stdout.trim()); // Выводим "сырой" вывод Cypress
+  process.exit(error ? 1 : 0); // Возвращаем код ошибки
 });
