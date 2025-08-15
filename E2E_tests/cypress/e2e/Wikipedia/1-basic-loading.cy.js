254ab081257e18f583f43de2fb2c659a4a9b8d4d
Ivan Popov <ikpopov@avito.ru>
2025-08-15 21:26:15 +0300

    test: revert expect error

--- E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
+++ E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
@@ -3,6 +3,6 @@ describe("Wikipedia Smoke Test", () => {
     // Просто проверяем, что страница загружается
     cy.visit("https://ru.wikipedia.org", { timeout: 10000 })
       .title() // Проверяем title страницы
-      .should("include", "Википедия1");
+      .should("include", "Википедия");
   });
 });
