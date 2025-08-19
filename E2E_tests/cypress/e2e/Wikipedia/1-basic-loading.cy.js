33de038aa8465e4a315fbddb2ee445dfb53b4eb7
Ivan Popov <ikpopov@avito.ru>
2025-08-19 20:47:40 +0300

    cresh 1 test

--- E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
+++ E2E_tests/cypress/e2e/Wikipedia/1-basic-loading.cy.js
@@ -3,6 +3,6 @@ describe("Wikipedia Smoke Test", () => {
     // Просто проверяем, что страница загружается
     cy.visit("https://ru.wikipedia.org", { timeout: 10000 })
       .title() // Проверяем title страницы
-      .should("include", "Википедия");
+      .should("include", "Википедия1");
   });
 });
